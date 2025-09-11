ab5247b8ab301ce4d1bc9505dcc8487382899a97
unknown <ben.arayag@duocuc.cl>
2025-09-11 18:35:03 -0300

    commit
    Se modifica carrito, producto, infoProducto js, y ifoProducto.html

--- Tienda Online Huerto Hogar/src/assets/js/infoProducto.js
+++ Tienda Online Huerto Hogar/src/assets/js/infoProducto.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const products = [
         { id: 1, name: 'Manzanas Fuji', price: 1200, image: '../Img/manzanas_fuji.jpg', description: 'Una manzana crujiente y dulce, perfecta para cualquier momento del día.' },
-        { id: 2, name: 'Naranjas Valencia', price: 100, image: '../Img/Naranjas Valencia.jpg', description: 'Naranjas jugosas y llenas de vitamina C, ideales para zumos.' },
-        { id: 3, name: 'Banana Cavendish', price: 1200, image: '../Img/banana cavendish.jpg', description: 'Bananas cremosas y energéticas, un snack natural y saludable.' },
-        { id: 4, name: 'Zanahoria Organica', price: 100, image: '../Img/Zhanaoria Organica.jpg', description: 'Zanahorias orgánicas, frescas y llenas de nutrientes.' },
-        { id: 5, name: 'Espinacas Frescas', price: 100, image: '../Img/espinacas Frescas.jpg', description: 'Espinacas tiernas y sabrosas, perfectas para ensaladas o cocidas.' },
-        { id: 6, name: 'Pimientos Tricolor', price: 100, image: '../Img/Pimientos_Tricolor.jpg', description: 'Pimientos de tres colores, añaden sabor y color a tus platos.' },
-        { id: 7, name: 'Miel Organica', price: 100, image: '../Img/Miel Organica.jpg', description: 'Miel pura y orgánica, un endulzante natural y saludable.' },
-        { id: 8, name: 'Quinoa Organica', price: 100, image: '../Img/Quinoa Organica.jpg', description: 'Quinoa orgánica, un superalimento versátil y nutritivo.' },
-        { id: 9, name: 'Leche Entera', price: 100, image: '../Img/Leche Entera.jpg', description: 'Leche fresca y entera, rica en calcio y proteínas.' }
+        { id: 2, name: 'Naranjas Valencia', price: 1000, image: '../Img/Naranjas Valencia.jpg', description: 'Naranjas jugosas y llenas de vitamina C, ideales para zumos.' },
+        { id: 3, name: 'Banana Cavendish', price: 800, image: '../Img/banana cavendish.jpg', description: 'Bananas cremosas y energéticas, un snack natural y saludable.' },
+        { id: 4, name: 'Zanahoria Organica', price: 900, image: '../Img/Zhanaoria Organica.jpg', description: 'Zanahorias orgánicas, frescas y llenas de nutrientes.' },
+        { id: 5, name: 'Espinacas Frescas', price: 700, image: '../Img/espinacas Frescas.jpg', description: 'Espinacas tiernas y sabrosas, perfectas para ensaladas o cocidas.' },
+        { id: 6, name: 'Pimientos Tricolor', price: 1500, image: '../Img/Pimientos_Tricolor.jpg', description: 'Pimientos de tres colores, añaden sabor y color a tus platos.' },
+        { id: 7, name: 'Miel Organica', price: 5000, image: '../Img/Miel Organica.jpg', description: 'Miel pura y orgánica, un endulzante natural y saludable.' },
+        { id: 8, name: 'Quinoa Organica', price: 2000, image: '../Img/Quinoa Organica.jpg', description: 'Quinoa orgánica, un superalimento versátil y nutritivo.' },
+        { id: 9, name: 'Leche Entera', price: 3000, image: '../Img/Leche Entera.jpg', description: 'Leche fresca y entera, rica en calcio y proteínas.' }
     ];
 
     const mainContent = document.querySelector('.main-content');
